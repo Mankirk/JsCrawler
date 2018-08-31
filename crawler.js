99019326324196822a4895a4e0035543e5bafb52
Cristian Leu <cristianleu@WS-CJ-U2-1107.local>
2018-08-31 09:41:34 +0300

    update

---
+++ crawler.js
@@ -0,0 +1,16 @@
+const request = require( "request" );
+const cheerio  = require( "cheerio" );
+
+const getSiteData = ( error, response, body ) => {
+    const $ = cheerio.load( body );
+
+    const links = $( "a" );
+
+
+    links.each( ( index, link ) => {
+      console.log(link.attribs.href)
+    } )
+
+}
+
+request( "https://www.mediafax.ro", getSiteData )
