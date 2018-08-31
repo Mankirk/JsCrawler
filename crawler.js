63d725f89f4515227a502134eebe8676c06b0c22
Cristian Leu <cristianleu@WS-CJ-U2-1107.local>
2018-08-31 11:51:40 +0300

    added string handlers

--- crawler.js
+++ crawler.js
@@ -2,30 +2,76 @@ const request = require( "request" );
 const cheerio  = require( "cheerio" );
 
 
+const removeDuplicates = ( urls ) => {
+
+    const uniqueUrls = [];
+    urls.forEach( url => {
+
+      if( uniqueUrls.indexOf( url ) === -1 ) {
+        uniqueUrls.push( url );
+      }
+    } )
+
+    return uniqueUrls
+}
+
+const extractBaseURLs = ( urls ) => {
+    const baseURLS = urls.map( url => {
+      const split = url.split( "." )
+
+      const domainEnd = split[ 2 ] ? split[ 2 ].slice( 0, 2 ): "";
+
+      if( ( split[ 0 ] !== "http://www" && split[ 0 ] !== "https://www" )|| domainEnd !== "ro" ) {
+        return "BADURL";
+      }
+
+      const single = [ split[ 0 ], split[ 1 ], domainEnd ];
+      return single.join( "." );
+    } ).filter( ( url ) => url !== "BADURL" );
+
+    return removeDuplicates( baseURLS );
+}
+
 const extractStandaloneDomains = ( linkList ) => {
 
   const standaloneDomains = linkList.filter( ( link ) => {
     const re = new RegExp("^(http|https)://", "i");
     const isStandaloneDomain = re.test(link);
-    return isStandaloneDomain
+    return isStandaloneDomain;
   } )
 
-  return standaloneDomains;
+  return extractBaseURLs( standaloneDomains );
 }
 
 
-const getSiteData = ( error, response, body ) => {
-    const $ = cheerio.load( body );
+const allLinks = ["https://www.mediafax.ro"];
 
+const getSiteData = ( error, response, body ) => {
+
+    if( error ) {
+      return
+    }
+    
+    const $ = cheerio.load( body );
     const links = $( "a" );
 
     const linkList = [];
     links.each( ( index, link ) => {
       linkList.push( link.attribs.href );
     } )
+    console.log( "---BATCH---", extractStandaloneDomains( linkList ) );
+    const finalLinks = extractStandaloneDomains( linkList )
 
-    console.log( extractStandaloneDomains( linkList ) );
-
+    if( finalLinks.length !== 0 ) {
+      finalLinks.shift();
+      finalLinks.forEach( ( link ) => {
+        return request( link, getSiteData );
+      } )
+    }
 }
 
-request( "https://www.mediafax.ro", getSiteData )
+const startCrawler = ( error, response, body ) => {
+    getSiteData( error, response, body );
+}
+
+request( "http://www.prosport.ro", startCrawler )
