1ab2d47add11751ef89d32bd73e2026ce96f08fe
Cristian Leu <cristianleu@WS-CJ-U2-1107.local>
2018-08-31 09:56:42 +0300

    WIP

--- crawler.js
+++ crawler.js
@@ -1,15 +1,30 @@
 const request = require( "request" );
 const cheerio  = require( "cheerio" );
 
+
+const extractStandaloneDomains = ( linkList ) => {
+
+  const standaloneDomains = linkList.filter( ( link ) => {
+    const re = new RegExp("^(http|https)://", "i");
+    const isStandaloneDomain = re.test(link);
+    return isStandaloneDomain
+  } )
+
+  return standaloneDomains;
+}
+
+
 const getSiteData = ( error, response, body ) => {
     const $ = cheerio.load( body );
 
     const links = $( "a" );
 
-
+    const linkList = [];
     links.each( ( index, link ) => {
-      console.log(link.attribs.href)
+      linkList.push( link.attribs.href );
     } )
+
+    console.log( extractStandaloneDomains( linkList ) );
 
 }
 
